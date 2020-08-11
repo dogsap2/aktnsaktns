23c744bafe98b3c084f30aa13e906ecb1aed9f6a
dk4750 <dk4750@naver.com>
2020-08-11 11:38:20 +0900

    포트폴리오 css 수정

---
+++ aktnsaktns/src/main/java/com/cafe24/aktnsaktns/aktnsaktns/AktnsaktnsApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.aktnsaktns.aktnsaktns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktnsaktnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AktnsaktnsApplication.class, args);
+	}
+
+}
---
+++ aktnsaktns/src/test/java/com/cafe24/aktnsaktns/aktnsaktns/AktnsaktnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.aktnsaktns.aktnsaktns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AktnsaktnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
